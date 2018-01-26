36744a915c507b9f55dbffedbb11507b621e3a34
MartinAntonio123 <a01701167@itesm.mx>
2018-01-26 08:43:05 -0600

    entrega ejercicio 1

---
+++ Lab 5/lab5_1.js
@@ -0,0 +1,11 @@
+function validar(){
+    var password = document.formula.pw1.value;
+    var password2 = document.formula.pw2.value;
+    if(password == password2){
+        alert("password valido")
+    }
+    else{
+        alert("password invalido")
+    }
+}
+contras1 = document.getElementById("contra1")
