0a547d4f10b0da205e520fbc8318362a7198e7fe
MartinAntonio123 <a01701167@itesm.mx>
2018-04-08 16:41:37 -0500

    lab 22 integracion con servicios de terceros

---
+++ Lab 22 integracion con servicios web/activities.php
@@ -0,0 +1,6 @@
+<?php include("partials/_header.html"); ?>
+<?php include("partials/_top_bar.html"); ?>
+<br /><br />
+
+<?php include("html/activities.html");?>
+<?php include("partials/_footer.html"); ?>
